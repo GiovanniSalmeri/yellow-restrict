1ebe3af6b034c52c76a80d07db28cf503d96eada
Giovanni <g.salmeri@mondodomani.org>
2021-02-14 14:59:51 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# yellow-restrict
-Restrict access to pages
+Restrict 0.8.16
+=============
+Restrict access to pages.
+
+<p align="center"><img src="restrict-screenshot.png?raw=true" width="795" height="594" alt="Screenshot"></p>
+
+## How to restrict the access to one or more pages
+
+Add a `Restrict` setting at the top of a page to protect it and its descendant pages (top pages are considered children of the home page). The value of the setting is a comma separated list of users and/or groups that may access the page with their credentials. The names of groups are prepended with `#`.
+
+Usernames, passwords and groups are defined in a file `system/extensions/restrict.ini` with the following syntax:
+
+```
+username : password : #group1, #group2...
+```
+
+You can specify for each username zero or more groups it belongs to. All usernames belong moreover to an implicit group `#all`. Do not use spaces or `:` in usernames and passwords, or begin usernames with `#`.
+
+This extension relies on the [HTTP basic authentication](https://en.wikipedia.org/wiki/Basic_access_authentication). For sensitive data use it only over an encrypted connection (`https://`). For a simpler alternative there is the [Private extension](https://github.com/schulle4u/yellow-extensions-schulle4u/tree/master/private).
+
+## Examples
+
+Page with restricted access:
+
+```
+---
+Title: Reserved page
+Restrict: john, #admin, #commitee
+---
+
+This page is reserved to John, administrators and members of the Commitee.
+```
+
+Usernames, passwords and groups in `restrict.ini`:
+
+```
+john : a2S%iZhK
+mary : cAaRWC8& : #commitee, #admin
+ben : 5DgZAC&R : #member
+lucy : ZWKD(8Jy : #admin, #member
+antony : 79wB5w@Z : #commitee
+mark : 9F5)F57e : #member
+```
+
+Passwords are stored in cleartext. Do not use them for any other purpose and transmit them securely to users.
+
+## Settings
+
+The following setting can be configured in file `system/settings/system.ini`:
+
+`RestrictUserFile` (default: `restrict.ini`) = filename for users, passwords and groups  
+
+For the restricted pages to show the username and a tip for "logging out", add the following line to the file `system/layouts/footer.html`:
+
+```
+<?php echo $this->yellow->page->getExtra("logout"); ?>
+```
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-restrict/archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-restrict/issues).
---
+++ restrict.php
@@ -0,0 +1,77 @@
+<?php
+// Restrict extension, https://github.com/GiovanniSalmeri/yellow-restrict
+
+class YellowRestrict {
+    const VERSION = "0.8.16";
+    public $yellow;         //access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+        $this->yellow->system->setDefault("restrictUserFile", "restrict.ini");
+    }
+
+    // Handle page meta data
+    public function onParseMeta($page) {
+        if ($page->isExisting("restrict")) {
+            $page->set("Description", $this->yellow->language->getTextHtml("RestrictDescription"));
+        }
+    }
+    
+    // Handle page layout
+    public function onParsePageLayout($page, $name) {
+        $masterPage = $page;
+        while ($masterPage and !$masterPage->isExisting("restrict")) {
+            $parentPage = $masterPage->getParent();
+            $homeLocation = $this->yellow->content->getHomeLocation($masterPage->getLocation());
+            if (!$parentPage && $masterPage->getLocation()!==$homeLocation) {
+                $masterPage = $this->yellow->content->find($homeLocation); // home as parent of top pages
+            } else {
+                $masterPage = $parentPage;
+            }
+        }
+        $allowedPeople = $masterPage ? $masterPage->get("restrict") : null;
+        if ($allowedPeople) {
+            if (!$this->matchPermissions($this->yellow->toolbox->getServer('PHP_AUTH_USER'), $this->yellow->toolbox->getServer('PHP_AUTH_PW'), $allowedPeople)) {
+                $this->yellow->page->setHeader("WWW-Authenticate", 'Basic realm="", charset="UTF-8"');
+                $this->yellow->page->error("401");
+            } else {
+                $page->set("RestrictUser", $this->yellow->toolbox->getServer('PHP_AUTH_USER'));
+            }
+        }
+    }
+
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="logout") {
+            if ($page->isExisting("restrict")) {
+                $output .= "<p>".str_replace("@user", "<b>".htmlspecialchars($page->get("RestrictUser"))."</b>", $this->yellow->language->getTextHtml("RestrictLogged"))."</p>\n";
+            }
+        }
+        return $output;
+    }
+
+    // Check whether user is allowed
+    function matchPermissions($givenUser, $givenPassword, $allowedPeople) {
+        $fileNameRestrict = $this->yellow->system->get("coreExtensionDirectory").$this->yellow->system->get("restrictUserFile");
+        $lines = @file($fileNameRestrict);
+        if ($lines) foreach ($lines as $line) {
+            if (trim($line)=="" or $line[0]=="#") continue;
+            list($user, $password, $groupsList) = array_map("trim", $this->yellow->toolbox->getTextList($line, ":", 3));
+            $groups = array_map("trim", explode(",", $groupsList));
+            $groups[] = "#all";
+            if ($user==$givenUser && $password==$givenPassword) {
+                foreach (array_map("trim", explode(",", $allowedPeople)) as $item) {
+                    if ($item=="") continue;
+                    if ($item[0]=="#") {
+                        if (in_array($item, $groups)) return true;
+                    } else {
+			if ($item==$user) return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
